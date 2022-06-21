fb7bec6bc67ad479cc842b8996c3182015c7d792
Sifiso-Dev <106865381+Sifiso-Dev@users.noreply.github.com>
2022-06-21 18:07:32 +0200

    Added last section that shows who the winner is

--- rock-papper-scissors.js
+++ rock-papper-scissors.js
@@ -17,7 +17,7 @@ computerSelection = computerPlay();
 
 // Function for one round between user and computer
 
-let playerSelection = ("Please choose between Rock, Papper or Scissor: ");
+let playerSelection = prompt("Please choose between Rock, Papper or Scissor: ");
 
 function playRound (playerSelection, computerSelection) {
     let result = "";
@@ -38,9 +38,41 @@ function playRound (playerSelection, computerSelection) {
         result = 'You win! Scissor beats papper!';
     }
 }    
+// A function to loop the playRound function 5 times
 
 function game () {
+    let scorePlayer = 0;
+    let scoreComp = 0;
 
+    for (let i = 0; i < 5; i++) {
+        playRound();
+
+        if (result == "It's a tie!!!") {
+            scorePlayer += 0;
+            scoreComp += 0;
+        } else if (result.slice(0, 7) == 'You win') {
+            scorePlayer += 1;
+            scoreComp += 0;
+        }else {
+            scorePlayer += 0;
+            scoreComp += 1;
+        }
+    }
+
+    return scoreComp;
+    return scorePlayer;
+
+}
+
+game();
+
+// Displaying who's the winner
+if (scorePlayer == scoreComp) {
+    console.log("No one won! It's a tie!");
+} else if (scorePlayer > scoreComp) {
+    console.log('Congatulations! You won!');
+} else {
+    console.log('You lost! The computer won!');
 }
 
 
