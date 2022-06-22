17b066b47b5ff11edf99789dfbad61ff12bb53ec
Sifiso-Dev <106865381+Sifiso-Dev@users.noreply.github.com>
2022-06-22 03:35:42 +0200

    Fixing errors

--- rock-papper-scissors.js
+++ rock-papper-scissors.js
@@ -1,48 +1,54 @@
 // Function for computer to randomly generate rock, papper or scissor
-let computerSelection = "";
+
 
 function computerPlay () {
     let randomNum = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
     if (randomNum == 1) {
-        return 'Rock';
+        return 'rock';
     } else if (randomNum == 2) {
-        return 'Papper';
+        return 'papper';
     } else {
-        return 'Scissor';
+        return 'scissor';
     }
 }
 
-computerSelection = computerPlay();
-
 // Function for one round between user and computer
 
-let playerSelection = prompt("Please choose between Rock, Papper or Scissor: ");
 
 function playRound (playerSelection, computerSelection) {
     let result = "";
 
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    if (playerSelection == computerSelection) {
         result = "It's a tie!!!";
-    } else if (playerSelection.toLowerCase() == 'papper' && computerSelection.toLowerCase() == 'rock') {
+    } else if (playerSelection == 'papper' && computerSelection == 'rock') {
         result = 'You win! Papper beats Rock!';
-    } else if (playerSelection.toLowerCase() == 'papper' && computerSelection.toLowerCase() == 'scissor') {
+    } else if (playerSelection == 'papper' && computerSelection == 'scissor') {
         result = 'You lose! Scissor beats Papper!';
-    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'papper') {
+    } else if (playerSelection == 'rock' && computerSelection == 'papper') {
         result = 'You lose! Papper beats Rock!';
-    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissor') {
+    } else if (playerSelection == 'rock' && computerSelection == 'scissor') {
         result = 'You win! Rock beats scissor!';
-    } else if (playerSelection.toLowerCase() == 'scissor' && computerSelection.toLowerCase() == 'rock') {
+    } else if (playerSelection == 'scissor' && computerSelection == 'rock') {
         result = 'You lose! Rock beats scissor!';
-    } else if (playerSelection.toLowerCase() == 'scissor' && computerSelection.toLowerCase() == 'papper') {
+    } else if (playerSelection == 'scissor' && computerSelection == 'papper') {
         result = 'You win! Scissor beats papper!';
     }
+
+    return result;
 }    
 // A function to loop the playRound function 5 times
 
-function game () {
+function game (playerSelection, computerSelection) {
     let scorePlayer = 0;
     let scoreComp = 0;
+    let result = "";
+
+    let userSelection = prompt("Please choose between Rock, Papper or Scissor: ");
+    let playerSelection = userSelection.toLocaleLowerCase();
+
+    let computerSelection = "";
+    computerSelection = computerPlay();
 
     for (let i = 0; i < 5; i++) {
         playRound();
